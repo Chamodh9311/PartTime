53da835a06db648bfc59c8dbd4e1186eaa34b3c5
chamodh93 <chamodh93@live.com>
2020-05-03 02:00:49 +0530

    add exp handling

--- PartTimeV1/Controllers/AdminController.cs
+++ PartTimeV1/Controllers/AdminController.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace PartTimeV1.Controllers
 {
-    public class AdminController : Controller
+    public class AdminController : BaseController
     {
-        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
-
-        // GET: Admin
         public ActionResult Index()
         {
-            Logger.Error("Goodbye cruel world");
-            return View();
+            try
+            {
+                throw new NullReferenceException();
+                return View();
+            }
+            catch (Exception exp)
+            {
+                Logger.Error(exp, "Goodbye cruel world");
+                throw exp;
+            }
+        
         }
     }
 }
---
+++ PartTimeV1/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace PartTimeV1.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        protected override void OnException(ExceptionContext filterContext)  
+        {
+            ViewResult view = new ViewResult();  
+            view.ViewName = "Error";  
+            filterContext.Result = view;  
+            filterContext.ExceptionHandled = true;
+
+            if (filterContext != null && filterContext.Exception != null)
+            {
+                Logger.Error(filterContext.Exception);
+            }
+        }
+    }
+}
