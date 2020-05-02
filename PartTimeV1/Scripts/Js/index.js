367b5be052b067ca36f8e40c76c5bfe7e6500894
chamodh93 <chamodh93@live.com>
2020-05-02 23:46:33 +0530

    finish profile UI

---
+++ PartTimeV1/Scripts/Js/index.js
@@ -0,0 +1,56 @@
+﻿$(function () {
+    //Initialize Select2 Elements
+    $('.select2').select2()
+});
+
+//Show hide I am section
+$(document).ready(function () {
+    $('#student').change(function () {
+        if ($(this).is(":checked")) {
+            $('#studentdetails').fadeIn('slow');
+            $("#employee").attr("disabled", "disabled").off('click');
+            $("#employeedetails").attr("disabled", "disabled").off('click');
+            $("#parttime").attr("disabled", "disabled").off('click');
+        }
+        else {
+            $('#studentdetails').fadeOut('slow');
+            $("#employee").removeAttr('disabled');
+            $("#employeedetails").removeAttr('disabled');
+            $("#parttime").removeAttr('disabled');
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#employee').change(function () {
+        if ($(this).is(":checked")) {
+            $('#employeedetails').fadeIn('slow');
+            $("#student").attr("disabled", "disabled").off('click');
+            $("#studentdetails").attr("disabled", "disabled").off('click');
+            $("#parttime").attr("disabled", "disabled").off('click');
+        }
+        else {
+            $('#employeedetails').fadeOut('slow');
+            $("#student").removeAttr('disabled');
+            $("#studentdetails").removeAttr('disabled');
+            $("#parttime").removeAttr('disabled');
+        }
+    });
+});
+
+$(document).ready(function () {
+    $('#parttime').change(function () {
+        if ($(this).is(":checked")) {
+            $("#student").attr("disabled", "disabled").off('click');
+            $("#studentdetails").attr("disabled", "disabled").off('click');
+            $("#employee").attr("disabled", "disabled").off('click');
+            $("#employeedetails").attr("disabled", "disabled").off('click');
+        }
+        else {
+            $("#student").removeAttr('disabled');
+            $("#studentdetails").removeAttr('disabled');
+            $("#employee").removeAttr('disabled');
+            $("#employeedetails").removeAttr('disabled');
+        }
+    });
+});
