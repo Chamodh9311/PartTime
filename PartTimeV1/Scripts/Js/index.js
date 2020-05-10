a48b471e6a7d69e17e7f4c2491ba92c75321eafd
chamodh93 <chamodh93@live.com>
2020-05-10 18:55:44 +0530

    add two tabs layout

--- PartTimeV1/Scripts/Js/index.js
+++ PartTimeV1/Scripts/Js/index.js
@@ -100,6 +100,27 @@ $(function () {
 
 });
 
+window.onload = function () {
+    document.getElementById("tabLoad").click();
+};
+
+
+//Tab Open
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
 //Show hide I am section
 $(document).ready(function () {
     $('#student').change(function () {
